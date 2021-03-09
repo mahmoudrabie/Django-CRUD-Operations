9cad059ee3316213f5083a68cf15a104a151dc16
mahmoudrabie <mahmoudrabie2004@gmail.com>
2021-03-09 17:33:20 +0300

    Translation Configurations

--- employee_project/employee_project/urls.py
+++ employee_project/employee_project/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('',include('employee_register.urls')),
