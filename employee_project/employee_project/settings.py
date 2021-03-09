d69d8cf246a299bce2348ff91109629bb90ae15d
mahmoudrabie <mahmoudrabie2004@gmail.com>
2021-03-09 17:29:27 +0300

    Add LANGUAGES to settings.py

--- employee_project/employee_project/settings.py
+++ employee_project/employee_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -111,7 +112,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ar' #'en-us'
+
+LANGUAGES = (
+    ('en', _('English')),
+    ('ar', _('Arabic')),
+    ('ta', _('Tamil')),
+)
+
 
 TIME_ZONE = 'UTC'
 
